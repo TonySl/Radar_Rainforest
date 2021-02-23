8b8db71ef651f352158d51592b7d31d454c9a303
Shengli Tao <sltao1990@gmail.com>
2021-02-23 12:09:38 +0100

    Update Step1_Americas_ASCAT_vs_QSCAT.m

--- Step1_Americas_ASCAT_vs_QSCAT.m
+++ Step1_Americas_ASCAT_vs_QSCAT.m
@@ -1,6 +1,6 @@
 %% Project Radar_Rainforest
 % Until 2021.02, there has been no well-calibrated C-band microwave (either active or passive) data set covering a long time span. 
-% There have been C-band passive microwave data from AMSR-E and AMSR2, but they didn't overlap temporally, thus have not been well calibrated (Du et al. 2017. Earth Syst. Sci. Data 9,
+% There have been passive microwave data from AMSR-E and AMSR2, but they didn't overlap temporally, thus have not been well calibrated (Du et al. 2017. Earth Syst. Sci. Data 9,
 % 791–808; Moesinger et al. 2020. Earth Syst. Sci. Data 12, 177–196). 
 % The aim of Project Radar_Rainforest is to fill this knowledge gap, by providing a fully harmonized, long-term (since 1992) C-band radar data set for global land areas, especially for tropical rainforests.
 
